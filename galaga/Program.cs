57ce48459fee9fa325c334a1603e775ffca38f7c
KDS5601 <igs8833@hotmail.com>
2019-08-10 19:51:25 +0900

    클래스 다형화

--- galaga/Program.cs
+++ galaga/Program.cs
@@ -9,15 +9,16 @@ namespace Gallag
         {
             Console.SetWindowSize(40, 30);
             Console.CursorVisible = false;
+
             Player player = new Player();
+
+            Thread InputThread = new Thread(player.KeyInput);
+            InputThread.Start();
+            InputThread.IsBackground = true;
 
             while (true)
             {
                 Draw();
-
-                Thread InputThread = new Thread(player.KeyInput);
-                InputThread.IsBackground = true;
-                InputThread.Start();
 
                 Thread.Sleep(30);
             }
@@ -25,36 +26,68 @@ namespace Gallag
             void Draw()
             {
                 Console.Clear();
-                Console.SetCursorPosition(player.Position.X, player.Position.Y);
-                Console.Write(player.Shape);
+
+                (int x, int y) postion = player.GetPostion();
+                Console.SetCursorPosition(postion.x , postion.y);
+                Console.Write(player.GetShape());
             }
         }
     }
 
-    class Player
+    abstract class Dot
     {
-        public (int X, int Y) Position;
+        protected enum Movement { Up, Down, Left, Right}
 
-        public string Shape = "■";
+        protected (int X, int Y) Position;
+        protected string Shape;
 
+        public (int x, int y) GetPostion ()
+        {
+            return Position;
+        }
+
+        public string GetShape()
+        {
+            return Shape;
+        }
+
+        protected void Moving (Movement movement)
+        {
+            if (movement == Movement.Right)
+                Position.X++;
+            else if (movement == Movement.Left && Position.X > 0)
+                Position.X--;
+            else if (movement == Movement.Up && Position.Y > 0)
+                Position.Y--;
+            else if (movement == Movement.Down)
+                Position.Y++;
+        }
+    }
+
+    class Player : Dot
+    {
         public Player()
         {
+            Shape = "-ㅅ-";
             Position.X = 0;
             Position.Y = 0;
         }
 
         public void KeyInput()
         {
-            ConsoleKeyInfo keys = Console.ReadKey(true);
+            for (; ; )
+            {
+                ConsoleKeyInfo keys = Console.ReadKey(true);
 
-            if (keys.Key == ConsoleKey.RightArrow)
-                Position.X++;
-            else if (keys.Key == ConsoleKey.LeftArrow)
-                Position.X--;
-            else if (keys.Key == ConsoleKey.UpArrow)
-                Position.Y--;
-            else if (keys.Key == ConsoleKey.DownArrow)
-                Position.Y++;
+                if (keys.Key == ConsoleKey.RightArrow)
+                    Moving(Movement.Right);
+                else if (keys.Key == ConsoleKey.LeftArrow)
+                    Moving(Movement.Left);
+                else if (keys.Key == ConsoleKey.UpArrow)
+                    Moving(Movement.Up);
+                else if (keys.Key == ConsoleKey.DownArrow)
+                    Moving(Movement.Down);
+            }
         }
     }
 }
