db1b849bf5becb134798823ca058028008c46a9c
김대성 <daeseong@gaonmaru.kr>
2019-08-30 19:39:09 +0900

    버전 123 분리

---
+++ SpeedTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SpeedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime time = new DateTime();
+
+            int[,] testMetrix = new int[1000000,1000000];
+
+            time = DateTime.Now;
+            
+            for (int i = 0; i < testMetrix.GetLength(0); i++)
+                for(int j = 0; j <testMetrix.GetLength(1); j++)
+                {
+                    testMetrix[i, j] = 0;
+                }
+            Console.WriteLine("뒷차원 부터 접근 : " + (time - DateTime.Now).ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
--- galaga/ScreenBuffer.cs
+++ galaga/ScreenBuffer.cs
@@ -6,6 +6,101 @@ using System.Collections.Generic;
 
 namespace Gallag
 {
+    public class ScreenBuffer1
+    {
+        private char[,] ScreenArry;
+
+        public ScreenBuffer1(int Height, int Width)
+        {
+            ScreenArry = new char[Height, Width];
+        }
+
+        public void Push(int x, int y, char char_p)
+        {
+            ScreenArry[y, x] = char_p;
+        }
+
+        public void Paint()
+        {
+            Console.Clear();
+
+            for (int i = 0; i < ScreenArry.GetLength(0); i++)
+            {
+                for (int j = 0; j < ScreenArry.GetLength(1); j++)
+                {
+                    Console.SetCursorPosition(j, i);
+                    Console.Write(ScreenArry[i, j]);
+                }
+            }
+
+            for (int i = 0; i < ScreenArry.GetLength(0); i++)
+            {
+                for (int j = 0; j < ScreenArry.GetLength(1); j++)
+                {
+                    ScreenArry[i, j] = ' ';
+                }
+            }
+        }
+    }
+
+    public class ScreenBuffer2
+    {
+
+        private List<(int x, int y, char c)> ScreensTuple = new List<(int x, int y, char c)>();
+
+
+        public void Push(int x, int y, char char_p)
+        {
+            ScreensTuple.Add((x, y, char_p));
+        }
+
+        public void Paint()
+        {
+            Console.Clear();
+
+            foreach ((int x, int y, char c) screensTuple in ScreensTuple)
+            {
+                Console.SetCursorPosition(screensTuple.x, screensTuple.y);
+                Console.Write(screensTuple.c);
+            }
+
+            ScreensTuple.Clear();
+        }
+
+    }
+
+    public class ScreenBuffer3
+    {
+        private char[,] ScreenArry;
+
+        public ScreenBuffer3(int Height, int Width)
+        {
+            ScreenArry = new char[Height, Width];
+        }
+
+        public async void Paint()
+        {
+            Console.Clear();
+
+            for (int i = 0; i < ScreenArry.GetLength(0); i++)
+            {
+                for (int j = 0; j < ScreenArry.GetLength(1); j++)
+                {
+                    Console.SetCursorPosition(j, i);
+                    Console.Write(ScreenArry[i, j]);
+                }
+            }
+
+            for (int i = 0; i < ScreenArry.GetLength(0); i++)
+            {
+                for (int j = 0; j < ScreenArry.GetLength(1); j++)
+                {
+                    ScreenArry[i, j] = ' ';
+                }
+            }
+        }
+    }
+
     public class ScreenBuffer
     {
         private List<(int x, int y, char c)> ScreensTuple = new List<(int x, int y, char c)>();
@@ -13,7 +108,7 @@ namespace Gallag
 
         public void Push(int x, int y, char char_p)
         {
-            ScreensTuple.Add((x,y,char_p));
+            ScreensTuple.Add((x, y, char_p));
         }
 
         public void Paint()
